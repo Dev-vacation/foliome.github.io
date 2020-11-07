c7b8d09016442a5013c2a1f7af5e39e0f3468c75
MoreThanWord <Dev-vacation@fontend.github.com>
2020-11-07 19:56:32 +0700

    Add somefunction
    add navigation menu
    add js to control the navigation menu
    and some image and svg code

---
+++ js/nav.js
@@ -0,0 +1,16 @@
+const navToggle1 = document.querySelector('.menu-button')
+const nav = document.querySelector('nav')
+const containerAll = document.querySelector('.container-all')
+
+const contAllStyle = containerAll.style
+const bodyClassList = document.body.classList
+
+navToggle1.addEventListener('click', _=> {
+  contAllStyle.transition ='transform 250ms ease-in-out'
+  bodyClassList.toggle('nav-is-open')
+})
+
+nav.addEventListener('click', _=> {
+  contAllStyle.transition ='0ms'
+  bodyClassList.remove('nav-is-open')
+})
